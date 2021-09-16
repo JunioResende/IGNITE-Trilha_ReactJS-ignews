ea54b225d758ea7a20ef7415934a647494300c81
JunioResende <junioresende23@gmail.com>
2021-09-16 15:14:26 -0300

    auth github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [ session ] = useSession()
+
+  console.log(session)
   
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
     type="button"
     className={styles.signInButton}
+    onClick={() => signOut()}
   >
     <FaGithub color="#04d361" />
     Junio Resende
@@ -19,6 +23,7 @@ export function SignInButton() {
     <button 
     type="button"
     className={styles.signInButton}
+    onClick={() => signIn('github')}
   >
     <FaGithub color="#eba417" />
     Sign in with GitHub
