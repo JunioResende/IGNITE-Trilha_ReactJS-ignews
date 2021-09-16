ecd877508f0dd526a8ceff57b82423ee3e29f979
JunioResende <junioresende23@gmail.com>
2021-09-16 12:34:13 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JaJrwAEJw3K6QJQh6O15mdS')
 
   const product = {
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   } 
 }
